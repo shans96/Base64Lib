1b20c69fc276679d8f91ecfefbb3faf59fdc50dc
shans96 <shanswanlow96@gmail.com>
2019-06-27 12:41:34 +0200

    Fix encoding errors when given a byte of data

--- include/base64lib/base64_encode.cpp
+++ include/base64lib/base64_encode.cpp
@@ -45,7 +45,7 @@ std::string encode_to_base64(const char *source_bytes,
 	else if (pad_result == padded_twice)
 	{
 		encoded_bytes.back() = '=';
-		encoded_bytes.at(padded_bytes.back() - 1) = '=';
+		encoded_bytes.at(encoded_bytes.length() - 2) = '=';
 	}
 	return encoded_bytes;
 }
@@ -58,13 +58,13 @@ pad_type pad(std::vector<unsigned char> *vector)
 	}
 	else if ((vector->size() + 1) % 3 == 0)
 	{
-		vector->push_back('=');
+		vector->push_back('\0');
 		return padded_once;
 	}
 	else if ((vector->size() + 2) % 3 == 0)
 	{
-		vector->push_back('=');
-		vector->push_back('=');
+		vector->push_back('\0');
+		vector->push_back('\0');
 		return padded_twice;
 	}
 }
